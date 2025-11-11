fbeba62bc83d4f86fb14450573f5d7c577c75c94
julia-ctp <juliactp.santos@gmail.com>
2025-11-11 19:11:47 -0300

    feat: login page interface

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,33 @@
+import { chessHallLogo } from "@/assets";
 import LoginComponent from "@/components/Login";
+import { Box, Flex, Image, Stack } from "@chakra-ui/react";
 
 function Login() {
-  return <LoginComponent />;
+  return (
+    <Flex
+      direction={{ base: "column", md: "row" }}
+      bg={"#020202"}
+      h={"100svh"}
+      w={"full"}
+      align={"center"}
+      justify={{ base: "space-between", md: "center" }}
+      gap={{ md: 14, lg: 24 }}
+      p={{ md: 12 }}
+    >
+      <Box display={"block"} my={"auto"}>
+        <Image
+          src={chessHallLogo.src}
+          fit={"contain"}
+          w={"auto"}
+          maxH={{ base: "240px", sm: "280px", md: "300px" }}
+        >
+        </Image>
+      </Box>
+      <Stack w={"full"} maxW={{ sm: "400px", md: "420px" }}>
+        <LoginComponent />
+      </Stack>
+    </Flex>
+  );
 }
 
 export default Login;
